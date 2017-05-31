7e611bbbd48d1ee92c511dc88336b065d07dffcb
Rafael Fernández López <ereslibre@ereslibre.es>
2017-05-31 21:00:46 +0200

    Write file to disk (/storage1 volume only for now)

--- app/controllers/api/v1/objects_controller.rb
+++ app/controllers/api/v1/objects_controller.rb
@@ -3,9 +3,10 @@ class Api::V1::ObjectsController < ApplicationController
   before_action :has_permissions?
 
   def create
-    object = params[:object][:payload]
-    File.open("/tmp/upload", "w") do |f|
-      f.write object.read
+    object = params[:object][:payload].read
+    object_name = Digest::SHA256.hexdigest object
+    File.open("/storage1/#{object_name}", "w") do |f|
+      f.write object
     end
   end
 
